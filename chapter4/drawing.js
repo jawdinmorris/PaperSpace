32bf3af46a0a9de1c2d52a7e9b314420a6e8b673
jawdinmorris <jordanandrewmorris1@gmail.com>
2019-10-16 21:00:03 +1000

    chapter 4 complete

---
+++ chapter4/drawing.js
@@ -0,0 +1,134 @@
+function draw_grid(ctx, minor, major, stroke, fill) {
+    minor = minor || 10;
+    major = major || minor * 5;
+    stroke = stroke || "#00FF00";
+    fill = fill || "#009900";
+
+    ctx.save();
+
+    ctx.strokeStyle = stroke;
+    ctx.fillStyle = fill;
+    let width = ctx.canvas.width,
+        height = ctx.canvas.height
+
+    for (var x = 0; x < width; x += minor) {
+        ctx.beginPath();
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, height);
+        ctx.lineWidth = (x % major == 0) ? 0.5 : 0.25;
+        ctx.stroke();
+        if (x % major == 0) {
+            ctx.fillText(x, x, 10);
+        }
+    }
+
+    for (var y = 0; y < height; y += minor) {
+        ctx.beginPath();
+        ctx.moveTo(0, y);
+        ctx.lineTo(width, y);
+        ctx.lineWidth = (y % major == 0) ? 0.5 : 0.25;
+        ctx.stroke();
+        if (y % major == 0) {
+            ctx.fillText(y, 0, y + 10);
+        }
+    }
+
+    ctx.restore();
+}
+
+function draw_ship(ctx, radius, options) {
+    options = options || {};
+    let angle = (options.angle || 0.5 * Math.PI) / 2;
+    // this is new
+    let curve1 = options.curve1 || 0.25;
+    let curve2 = options.curve2 || 0.75;
+    ctx.save();
+    if (options.guide) {
+        ctx.strokeStyle = "white";
+        ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+        ctx.lineWidth = 0.5;
+        ctx.beginPath();
+        ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.fill();
+    }
+    ctx.lineWidth = options.lineWidth || 2;
+    ctx.strokeStyle = options.stroke || "white";
+    ctx.fillStyle = options.fill || "black";
+    ctx.beginPath();
+    ctx.moveTo(radius, 0);
+    // here we have the three curves
+    ctx.quadraticCurveTo(
+        Math.cos(angle) * radius * curve2,
+        Math.sin(angle) * radius * curve2,
+        Math.cos(Math.PI - angle) * radius,
+        Math.sin(Math.PI - angle) * radius
+    );
+    ctx.quadraticCurveTo(-radius * curve1, 0,
+        Math.cos(Math.PI + angle) * radius,
+        Math.sin(Math.PI + angle) * radius
+    );
+    ctx.quadraticCurveTo(
+        Math.cos(-angle) * radius * curve2,
+        Math.sin(-angle) * radius * curve2,
+        radius, 0
+    );
+    ctx.fill();
+    ctx.stroke();
+
+    // a new guide line and circle show the control point (Getting complicated)
+    if (options.guide) {
+        ctx.strokeStyle = "white";
+        ctx.fillStyle = "white";
+        ctx.lineWidth = 0.5;
+        ctx.beginPath();
+        ctx.moveTo(
+            Math.cos(-angle) * radius,
+            Math.sin(-angle) * radius
+        );
+        ctx.lineTo(0, 0);
+        ctx.lineTo(
+            Math.cos(angle) * radius,
+            Math.sin(angle) * radius
+        );
+        ctx.moveTo(-radius, 0);
+        ctx.lineTo(0, 0);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(
+            Math.cos(angle) * radius * curve2,
+            Math.sin(angle) * radius * curve2,
+            radius / 40, 0, 2 * Math.PI
+        );
+        ctx.fill();
+        ctx.beginPath();
+        ctx.arc(
+            Math.cos(-angle) * radius * curve2,
+            Math.sin(-angle) * radius * curve2,
+            radius / 40, 0, 2 * Math.PI
+        );
+        ctx.fill();
+        ctx.beginPath();
+        ctx.arc(radius * curve1 - radius, 0, radius / 50, 0, 2 *
+            Math.PI);
+        ctx.fill();
+    }
+    ctx.restore();
+}
+
+function draw_pacman(ctx, x, y, radius, openValue) {
+    x = x || 200;
+    y = y || 200;
+    radius = radius || 150;
+    openValue = openValue / 2 || .2;
+    ctx = ctx;
+
+    ctx.beginPath();
+    ctx.arc(x, y, radius, (0 + openValue) * Math.PI, (2 - openValue) * Math.PI);
+    ctx.lineTo(x, y);
+    ctx.fillStyle = "yellow";
+    ctx.strokeStyle = "black";
+    ctx.fill();
+    ctx.closePath()
+    ctx.stroke();
+}
