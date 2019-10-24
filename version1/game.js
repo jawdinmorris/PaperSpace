4087947613931a7b0e3080e45bc06421245c2cf5
jawdinmorris <jordanandrewmorris1@gmail.com>
2019-10-24 16:46:58 +1000

    kongregate API loading before game

--- version1/game.js
+++ version1/game.js
@@ -5,15 +5,6 @@ function collision(obj1, obj2) {
 function distance_between(obj1, obj2) {
     return Math.sqrt(Math.pow(obj1.x - obj2.x, 2) + Math.pow(obj1.y - obj2.y, 2));
 }
-// Load the API
-var kongregate;
-kongregateAPI.loadAPI(onComplete);
-
-// Callback function
-function onComplete() {
-    // Set the global kongregate API object
-    kongregate = kongregateAPI.getAPI();
-}
 
 var bombSound = new Audio('bomb.wav');
 var laserSound = new Audio('laser.wav')
